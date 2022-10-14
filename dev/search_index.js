dac6b78862670715a02d8850806ddb02e2a970e6
Documenter.jl <documenter@juliadocs.github.io>
2022-10-14 06:27:31 +0000

    build based on 03a54fd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WGSLTypes","category":"page"},{"location":"#WGSLTypes","page":"Home","title":"WGSLTypes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WGSLTypes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WGSLTypes]","category":"page"}]
+}
